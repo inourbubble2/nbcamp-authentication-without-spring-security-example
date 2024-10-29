24aa8e7a1b7a7273e0bb1ed435e4b65cdfd4be38
Nayeon Kim <inourbubble2@gmail.com>
2024-10-29 19:06:32 +0900

    initial commit

---
+++ src/main/java/com/navi/nbcampauthenticationwithoutspringsecurityexample/NbcampAuthenticationWithoutSpringSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.navi.nbcampauthenticationwithoutspringsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampAuthenticationWithoutSpringSecurityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampAuthenticationWithoutSpringSecurityExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navi/nbcampauthenticationwithoutspringsecurityexample/NbcampAuthenticationWithoutSpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.nbcampauthenticationwithoutspringsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampAuthenticationWithoutSpringSecurityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
