9be31997bbe4c9add7f922cafa54d796dd2cf7e2
Nayeon Kim <inourbubble2@gmail.com>
2024-10-29 19:09:00 +0900

    feat: User Entity & Repository 추가

---
+++ src/main/java/com/navi/nbcampauthenticationwithoutspringsecurityexample/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.navi.nbcampauthenticationwithoutspringsecurityexample.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+}
